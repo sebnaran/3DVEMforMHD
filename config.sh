05025c2922b76f0844f6007d95d7a7ccac65bdcf
Sebastian <naranjos@oregonstate.edu>
2019-08-09 17:32:25 -0600

    add def of path names regarding trilinos libraries

--- config.sh
+++ config.sh
@@ -2,9 +2,10 @@
 
 
 rm -f CMakeCache.txt
-
+#! Before running the cmakelists.txt we will populate the cmakecache to define a series of variables
+#! Regarding the paths to the installation directories of the TPLs.
 cmake \
-  -C $HOME/Codes/amanzi/install/tpls/share/cmake/amanzi-tpl-config.cmake \
-  ./
+-C $HOME/Codes/amanzi/install/tpls/share/cmake/amanzi-tpl-config.cmake \
+./
 
 make VERBOSE=1
