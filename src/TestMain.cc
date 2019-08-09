d274a606628d841d88915fed1837f91534e4c4ec
Sebastian <naranjos@oregonstate.edu>
2019-08-08 21:20:44 -0600

    add elasticity test, modified scr/CMakeLists.txt to link elasticitytest

--- src/TestMain.cc
+++ src/TestMain.cc
@@ -1,11 +1,39 @@
 #include <UnitTest++.h>
 
-TEST(Sanity){
-  CHECK_EQUAL(1,1);
-}
+#include "elasticitytest.cc"
 
-int main(int argc, char *argv[])
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// TPLs
+#include "EpetraExt_RowMatrixOut.h"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_ParameterXMLFileReader.hpp"
+
+
+// Amanzi
+#include "MeshFactory.hh"
+#include "GMVMesh.hh"
+#include "LinearOperatorPCG.hh"
+#include "Tensor.hh"
+
+// Amanzi::Operators
+#include "PDE_Elasticity.hh"
+
+#include "AnalyticElasticity01.hh"
+#include "Verification.hh"
+
+//int main(){
+//  std::cout<<"Hello"<<std::endl;
+//  return 0;
+//}
+
+
+int main(int , const char *[])
 {
   return UnitTest::RunAllTests();
 }
-
