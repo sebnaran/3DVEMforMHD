8ee39037434d971dcea793c820f1e3e3d5670330
Sebastian <naranjos@oregonstate.edu>
2019-08-12 16:51:46 -0600

    trying to link libraries

--- config.sh
+++ config.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 
-rm -f CMakeCache.txt
+rm -rf CMakeCache.txt /build
 #! Before running the cmakelists.txt we will populate the cmakecache to define a series of variables
 #! Regarding the paths to the installation directories of the TPLs.
 cmake \
 -C $HOME/Codes/amanzi/install/tpls/share/cmake/amanzi-tpl-config.cmake \
+-D CMAKE_BUILD_TYPE="Release" \
+-D ENABLE_Structured:BOOL=false \
 ./
 
 make VERBOSE=1
--- src/TestMain.cc
+++ src/TestMain.cc
@@ -8,18 +8,21 @@
 #include <string>
 #include <vector>
 
+#include "mpi.h"
+
 // TPLs
 #include "EpetraExt_RowMatrixOut.h"
 #include "Teuchos_RCP.hpp"
 #include "Teuchos_ParameterList.hpp"
 #include "Teuchos_ParameterXMLFileReader.hpp"
-
+#include "Teuchos_GlobalMPISession.hpp"
 
 // Amanzi
 #include "MeshFactory.hh"
 #include "GMVMesh.hh"
 #include "LinearOperatorPCG.hh"
 #include "Tensor.hh"
+#include "VerboseObject_objs.hh"
 
 // Amanzi::Operators
 #include "PDE_Elasticity.hh"
@@ -32,8 +35,14 @@
 //  return 0;
 //}
 
-
-int main(int , const char *[])
+TEST(Sanity) 
 {
+   CHECK_EQUAL(1, 1);
+}
+
+
+int main(int argc, char *argv[])
+{
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv);
   return UnitTest::RunAllTests();
 }
